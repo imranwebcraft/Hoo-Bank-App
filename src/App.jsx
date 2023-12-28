bdbacd9dc0adaefec3da42bddaa9a3f3dab3ee15
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-28 19:07:26 +0600

    import assest and setup some basic style in index.css and in style.js file also define the whole website structure in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import style from './style';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	return (
+		<div className="bg-primary w-full overflow-hidden">
+			{/* Navbar Div */}
+			<div className={`${style.paddingX} ${style.flexCenter} text-white`}>
+				{/* Img */}
+				<div className={`${style.boxWidth}`}>Navbar</div>
+			</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+			{/* Hero section */}
+			<div className={`bg-primary ${style.flexStart}`}>
+				<div className={`${style.boxWidth} text-white`}>Hero Content</div>
+			</div>
 
-export default App
+			{/* Others Section section */}
+			<div className={`bg-primary ${style.flexStart} ${style.paddingX}`}>
+				<div className={`${style.boxWidth} text-white`}>
+					Stats Business Billing Car Deal Testimonial Client CTA Footer
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
