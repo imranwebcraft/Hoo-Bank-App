88bbdc6e4d69669ec52615eec58dbc7b345b3d33
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-29 01:50:26 +0600

    Client sectio compelted

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,24 @@
+import { clients } from '../constant';
+import styles from '../style';
+
 const Client = () => {
 	return (
-		<div>
-			<h3>Client</h3>
-		</div>
+		<section className={`${styles.flexCenter} my-4`}>
+			<div className={`${styles.flexCenter} gap-[5px] flex-wrap w-full`}>
+				{clients.map((client) => (
+					<div
+						key={client.id}
+						className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[100px]`}
+					>
+						<img
+							src={client.logo}
+							alt="client_log"
+							className="w-[100px] sm:w-[180px] object-contain"
+						/>
+					</div>
+				))}
+			</div>
+		</section>
 	);
 };
 
