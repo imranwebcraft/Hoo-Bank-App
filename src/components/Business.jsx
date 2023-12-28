5bfe4da1403c0da179b7676156dc4794795f7df2
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-29 00:33:58 +0600

    Custom button and business section is done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,56 @@
+import { features } from '../constant';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const FeatureCard = ({ icon, title, content, index }) => {
+	return (
+		<div
+			className={`flex flex-row p-6 rounded-[20px] ${
+				index === features.length - 1 ? 'mb-0' : 'mb-6'
+			} feature-card`}
+		>
+			<div
+				className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+			>
+				<img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+			</div>
+			<div className=" flex-1 flex flex-col ml-3">
+				<h3 className=" font-poppins font-semibold text-[18px] leading-[23px] mb-1">
+					{title}
+				</h3>
+				<p className=" font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+					{content}
+				</p>
+			</div>
+		</div>
+	);
+};
+
 const Business = () => {
 	return (
-		<div>
-			<h3>Business</h3>
-		</div>
+		<section id="features" className={layout.section}>
+			{/* Left Side */}
+			<div className={`${layout.sectionInfo}`}>
+				<h2 className={styles.heading2}>
+					You do the business, <br className="hidden sm:block" />
+					we’ll handle the money.
+				</h2>
+				<p className={`${styles.paragraph} max-w-[450px]`}>
+					With the right credit card, you can improve your financial life by
+					building credit, earning rewards and saving money. But with hundreds
+					of credit cards on the market.
+				</p>
+				<Button styles="mt-10" />
+			</div>
+			{/* Right Side */}
+			<div className="flex-1">
+				<div className={`${layout.sectionImg} flex-col`}>
+					{features.map((feature, index) => (
+						<FeatureCard key={feature.id} index={index} {...feature} />
+					))}
+				</div>
+			</div>
+		</section>
 	);
 };
 
