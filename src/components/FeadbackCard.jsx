13d5217058023653b637dcb6c1ea4be4dd900310
imranwebcraft <imranhossainsakil22@gmail.com>
2023-12-29 01:34:17 +0600

    Testimonial section added

--- src/components/FeadbackCard.jsx
+++ src/components/FeadbackCard.jsx
@@ -1,7 +1,31 @@
-const FeadbackCard = () => {
+import { quotes } from '../assets';
+
+const FeadbackCard = ({ card: { content, name, title, img } }) => {
 	return (
-		<div>
-			<h3>FeadbackCard</h3>
+		<div
+			className={`flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] mr-0 sm:mr-5 md:mr-10 my-5 feedback-card`}
+		>
+			<img
+				src={quotes}
+				alt="quote"
+				className="w-[42px] h-[27px] bg-contain mb-10"
+			/>
+			<p className=" font-poppins font-normal text-[18px] leading-[32px] mb-6 tracking-[0.35px]">
+				{content}
+			</p>
+			<div className=" flex flex-row">
+				<img
+					src={img}
+					alt="authorImage"
+					className="w-[48px] h-[48px] rounded-full"
+				/>
+				<div className="flex flex-col ml-4">
+					<h3 className="font-poppins font-normal text-[20px]">{name}</h3>
+					<p className=" font-poppins font-normal text-[16px] text-dimWhite">
+						{title}
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,8 +1,34 @@
+import styles from '../style';
+import { feedback } from '../constant';
+import FeadbackCard from './FeadbackCard';
+
 const Testimonial = () => {
 	return (
-		<div>
-			<h3>Testimonial</h3>
-		</div>
+		<section
+			id="clients"
+			className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+		>
+			{/* TODO: Gradient */}
+			<div className=" absolute z-[0] w-[60%] h-[60%] rounded-full blue__gradient -right-[50%]" />
+
+			<div
+				className={` w-full flex justify-between items-center flex-col md:flex-row mb-6 md:mb-16 relative z-[1]`}
+			>
+				<h4 className={styles.heading2}>
+					What people are <br className=" hidden md:block" /> saying about us
+				</h4>
+				<p className={`${styles.paragraph} max-w-[450px]`}>
+					Everything you need to accept card payments and grow your business
+					anywhere on the planet.
+				</p>
+			</div>
+
+			<div className=" flex flex-wrap justify-start sm:justify-center w-full feedback-container relative z-[1]">
+				{feedback.map((card) => (
+					<FeadbackCard key={card.id} card={card} />
+				))}
+			</div>
+		</section>
 	);
 };
 
